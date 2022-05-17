b1785ddb67cb5a699f426eaafd8db4aecca6c8a9
Felipe Alonso Martins <felipe.alonso@cross-compass.com>
2022-05-17 14:59:24 +0900

    Rename GreetService to CalcService

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from calculator.calculator_pb2 import CalcRequest, NumericResult
 from calculator import calculator_pb2_grpc
 
 
-class GreetService(calculator_pb2_grpc.CalculatorServicer):
+class CalcService(calculator_pb2_grpc.CalculatorServicer):
 
     def SumNumbers(self, request, context):
         return NumericResult(sum=request.numberA + request.numberB)
@@ -16,7 +16,7 @@ class GreetService(calculator_pb2_grpc.CalculatorServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     calculator_pb2_grpc.add_CalculatorServicer_to_server(
-        GreetService(), server
+        CalcService(), server
     )
     server.add_insecure_port("[::]:5000")
     server.start()
